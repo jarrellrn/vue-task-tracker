0ac5b459b1360d3607b59df27f95376f19aa3e1c
jarrellrn <jarrellrn@gmail.com>
2022-07-27 10:08:44 -0500

    added init onclick func

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,5 +1,5 @@
 <template>
-  <button :style="{ background: color }"
+  <button @click="onClick()" :style="{ background: color }"
   class="btn">{{ text }}</button>
 </template>
 
@@ -10,5 +10,10 @@ export default {
     text: String,
     color: String,
   },
+  methods: {
+    onClick() {
+      console.log("click")
+    }
+  }
 }
 </script>
